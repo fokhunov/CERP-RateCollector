ede129ddfb001c1df3aa4171759e37fe05034f70
fokhunov <farrukh.okhunov@zattoo.com>
2020-02-10 23:31:49 +0100

    UZ : fix parsing

---
+++ src/parser/test_uz.py
@@ -0,0 +1,86 @@
+import logging
+import unittest
+
+from src.fetcher import Fetcher
+from src.parser import ParserUZ
+
+
+class TestParsingUzbekistan(unittest.TestCase):
+
+    def setUp(self):
+        logger = logging.getLogger()
+        logger.setLevel("DEBUG")
+
+        self.parser = ParserUZ(logger, Fetcher())
+
+    # 1. Parse Central Bank of Uzbekistan (web page)
+    def test_parse_cbu(self):
+        rates = self.parser.parse_cbu()
+        self.assert_rates("cbu", rates)
+
+    # 2. Parse National Bank of Uzbekistan (web page)
+    def test_parse_nbu(self):
+        rates = self.parser.parse_nbu()
+        self.assert_rates("nbu", rates)
+
+    # 3. Parse KDB (web page)
+    def test_parse_kdb(self):
+        rates = self.parser.parse_kdb()
+        print("kdb:", rates)
+        self.assertIsNotNone(rates['usd_buy'])
+        self.assertIsNotNone(rates['usd_sale'])
+        self.assertIsNotNone(rates['eur_buy'])
+        self.assertIsNotNone(rates['eur_sale'])
+
+    # 4. Parse XB (web page)
+    def test_parse_xb(self):
+        rates = self.parser.parse_xb()
+        self.assert_rates("xb", rates)
+
+    # 5. TODO: FIX Parse Mikrokreditbank (web page)
+    def test_parse_mikrokreditbank(self):
+        rates = self.parser.parse_mikrokreditbank()
+        self.assert_rates("mikrokreditbank", rates)
+
+    # 6. TODO: FIX Parse Turonbank (web page)
+    def test_parse_turonbank(self):
+        rates = self.parser.parse_turonbank()
+        self.assert_rates("turonbank", rates)
+
+    # 7. TODO: FIX Parse Aloqabank (web page)
+    def test_parse_aloqabank(self):
+        rates = self.parser.parse_aloqabank()
+        self.assert_rates("aloqabank", rates)
+
+    # 8. Parse Aab (web page)
+    def test_parse_aab(self):
+        rates = self.parser.parse_aab()
+        self.assert_rates("aab", rates)
+
+    # 9. Parse Trustbank (web page)
+    def test_parse_trustbank(self):
+        rates = self.parser.parse_trustbank()
+        self.assert_rates("trustbank", rates)
+
+    # 10. TODO: FIX Parse Ipakyulibank (web page)
+    def test_parse_ipakyulibank(self):
+        rates = self.parser.parse_ipakyulibank()
+        self.assert_rates("ipakyulibank", rates)
+
+    # 11. TODO: FIX Parse Savdogarbank (web page)
+    def test_parse_savdogarbank(self):
+        rates = self.parser.parse_savdogarbank()
+        self.assert_rates("savdogarbank", rates)
+
+    def assert_rates(self, bank, rates):
+        print(bank + ":", rates)
+        self.assertIsNotNone(rates['usd_buy'])
+        self.assertIsNotNone(rates['usd_sale'])
+        self.assertIsNotNone(rates['eur_buy'])
+        self.assertIsNotNone(rates['eur_sale'])
+        self.assertIsNotNone(rates['rub_buy'])
+        self.assertIsNotNone(rates['rub_sale'])
+
+
+if __name__ == "__main__":
+    TestParsingUzbekistan()
--- src/parser/uz.py
+++ src/parser/uz.py
@@ -19,10 +19,7 @@ class ParserUZ(base.Parser):
 
     # 1. Parse Central Bank of Uzbekistan (web page)
     def parse_cbu(self):
-        result = self.fetcher.fetch(
-            "http://www.cbu.uz/ru/"
-        )
-
+        result = self.fetcher.fetch("http://www.cbu.uz/ru/")
         try:
             context = BeautifulSoup(
                 result,
@@ -36,16 +33,13 @@ class ParserUZ(base.Parser):
                     parts = tags[i].getText().split()
                     code = parts[1]
                     value = rate.from_string(parts[3])
-
                     if code == "USD":
                         rates["usd_buy"] = rates["usd_sale"] = value
                     elif code == "EUR":
                         rates["eur_buy"] = rates["eur_sale"] = value
                     elif code == "RUB":
                         rates["rub_buy"] = rates["rub_sale"] = value
-
                 return rates
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -53,10 +47,7 @@ class ParserUZ(base.Parser):
 
     # Parse Central Bank of Uzbekistan all rates (web page)
     def parse_cbu_all(self):
-        result = self.fetcher.fetch(
-            "http://www.cbu.uz/ru/"
-        )
-
+        result = self.fetcher.fetch("http://www.cbu.uz/ru/")
         try:
             context = BeautifulSoup(
                 result,
@@ -69,7 +60,6 @@ class ParserUZ(base.Parser):
                 for i in range(len(tags)):
                     parts = tags[i].getText().split()
                     code = parts[1]
-
                     if code in currency.ALLOWED:
                         nominal = int(parts[0])
                         value = rate.from_string(parts[3])
@@ -78,9 +68,7 @@ class ParserUZ(base.Parser):
                             "nominal": nominal,
                             "value": value
                         }
-
                 return rates
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -88,10 +76,7 @@ class ParserUZ(base.Parser):
 
     # 2. Parse National Bank of Uzbekistan (web page)
     def parse_nbu(self):
-        result = self.fetcher.fetch(
-            "https://nbu.uz/en/exchange-rates/"
-        )
-
+        result = self.fetcher.fetch("https://nbu.uz/en/exchange-rates/")
         try:
             context = BeautifulSoup(
                 result,
@@ -102,24 +87,19 @@ class ParserUZ(base.Parser):
             if tags:
                 rates = {}
                 for tag in tags:
-                    tds = tag.findChildren('td')
-                    if len(tds) < 1:
+                    if re.search(r'USD', tag.text):
+                        rates["usd_buy"] = rate.from_string(tag.find_all('td')[2].getText())
+                        rates["usd_sale"] = rate.from_string(tag.find_all('td')[3].getText())
                         continue
-
-                    if tds[1].text == "USD":
-                        rates['usd_buy'] = rates['usd_sale'] = rate.from_string(tds[2].getText())
+                    if re.search(r'EUR', tag.text):
+                        rates["eur_buy"] = rate.from_string(tag.find_all('td')[2].getText())
+                        rates["eur_sale"] = rate.from_string(tag.find_all('td')[3].getText())
                         continue
-
-                    if tds[1].text == "EUR":
-                        rates['eur_buy'] = rates['eur_sale'] = rate.from_string(tds[2].getText())
+                    if re.search(r'RUB', tag.text):
+                        rates["rub_buy"] = rate.from_string(tag.find_all('td')[2].getText())
+                        rates["rub_sale"] = rate.from_string(tag.find_all('td')[3].getText())
                         continue
-
-                    if tds[1].text == "RUB":
-                        rates['rub_buy'] = rates['rub_sale'] = rate.from_string(tds[2].getText())
-                        continue
-
                 return rates
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -127,30 +107,27 @@ class ParserUZ(base.Parser):
 
     # 3. Parse KDB (web page)
     def parse_kdb(self):
-        result = self.fetcher.fetch(
-            "http://www.kdb.uz/#"
-        )
-
+        result = self.fetcher.fetch("https://kdb.uz/ru/interactive-services/exchange-rates")
         try:
             context = BeautifulSoup(
                 result,
                 'html.parser',
-                parse_only=SoupStrainer('div', {'class': re.compile(r'exchange_widget_text')})
+                parse_only=SoupStrainer(id="kdb")
             )
-            tags = context.find_all('tr')
-            if tags:
-
+            tags = context.find_all('td')
+            if tags and len(tags) > 2:
+                usd = tags[0].text.strip().split("/")
+                eur = tags[1].text.strip().split("/")
                 return {
-                    'usd_buy': rate.from_string(tags[1].findChildren('td')[1].getText()),
-                    'usd_sale': rate.from_string(tags[1].findChildren('td')[2].getText()),
-                    'eur_buy': rate.from_string(tags[2].findChildren('td')[1].getText()),
-                    'eur_sale': rate.from_string(tags[2].findChildren('td')[2].getText()),
+                    "usd_buy": rate.from_string(usd[0]),
+                    "usd_sale": rate.from_string(usd[1]),
+                    "eur_buy": rate.from_string(eur[0]),
+                    "eur_sale": rate.from_string(eur[1]),
 
                     # absent
                     # rates['rub_buy'] = string_to_int(tags.findChildren('td')[3].getText())
                     # rates['rub_sale'] = string_to_int(tags.findChildren('td')[4].getText())
                 }
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -158,37 +135,25 @@ class ParserUZ(base.Parser):
 
     # 4. Parse XB (web page)
     def parse_xb(self):
-        result = self.fetcher.fetch(
-            "http://www.xb.uz/ru/interactive/rates"
-        )
-
+        result = self.fetcher.fetch("http://www.xb.uz/ru")
         try:
             context = BeautifulSoup(
                 result,
                 'html.parser',
-                parse_only=SoupStrainer(id="currencies-table")
+                parse_only=SoupStrainer('div', {'class': re.compile(r'currency__table')})
             )
-            tags = context.find_all('tr')
+            tags = context.find_all('div', class_='currency__amount')
             if tags:
-                rates = {}
-                for tag in tags:
-                    if re.search(r'USD', tag.text):
-                        rates['usd_buy'] = rate.from_string(tag.findChildren('td')[4].getText())
-                        rates['usd_sale'] = rate.from_string(tag.findChildren('td')[5].getText())
-                        continue
-
-                    if re.search(r'EUR', tag.text):
-                        rates['eur_buy'] = rate.from_string(tag.findChildren('td')[4].getText())
-                        rates['eur_sale'] = rate.from_string(tag.findChildren('td')[5].getText())
-                        continue
-
-                    if re.search(r'RUB', tag.text):
-                        rates['rub_buy'] = rate.from_string(tag.findChildren('td')[4].getText())
-                        rates['rub_sale'] = rate.from_string(tag.findChildren('td')[5].getText())
-                        continue
-
-                return rates
-
+                buy = tags[0].find_all('div')
+                sale = tags[1].find_all('div')
+                return {
+                    "usd_buy": rate.from_string(buy[2].getText()),
+                    "usd_sale": rate.from_string(sale[2].getText()),
+                    "eur_buy": rate.from_string(buy[1].getText()),
+                    "eur_sale": rate.from_string(sale[1].getText()),
+                    "rub_buy": rate.from_string(buy[5].getText()),
+                    "rub_sale": rate.from_string(buy[5].getText()),
+                }
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -196,10 +161,7 @@ class ParserUZ(base.Parser):
 
     # 5. Parse Mikrokreditbank (web page)
     def parse_mikrokreditbank(self):
-        result = self.fetcher.fetch(
-            "https://mikrokreditbank.uz/ru/"
-        )
-
+        result = self.fetcher.fetch("https://mikrokreditbank.uz/ru/")
         try:
             context = BeautifulSoup(
                 result,
@@ -214,19 +176,15 @@ class ParserUZ(base.Parser):
                         rates['usd_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['usd_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                     if re.search(r'EUR', tag.text):
                         rates['eur_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['eur_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                     if re.search(r'RUB', tag.text):
                         rates['rub_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['rub_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                 return rates
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -234,10 +192,7 @@ class ParserUZ(base.Parser):
 
     # 6. Parse Turonbank (web page)
     def parse_turonbank(self):
-        result = self.fetcher.fetch(
-            "http://www.turonbank.uz/"
-        )
-
+        result = self.fetcher.fetch("http://www.turonbank.uz/")
         try:
             context = BeautifulSoup(
                 result,
@@ -252,19 +207,15 @@ class ParserUZ(base.Parser):
                         rates['usd_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['usd_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                     if re.search(r'EUR', tag.text):
                         rates['eur_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['eur_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                     if re.search(r'RUB', tag.text):
                         rates['rub_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['rub_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                 return rates
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -272,10 +223,7 @@ class ParserUZ(base.Parser):
 
     # 7. Parse Aloqabank (web page)
     def parse_aloqabank(self):
-        result = self.fetcher.fetch(
-            "http://www.aloqabank.uz/ru/page/interactive/rates"
-        )
-
+        result = self.fetcher.fetch("http://www.aloqabank.uz/ru/page/interactive/rates")
         try:
             context = BeautifulSoup(
                 result,
@@ -290,19 +238,15 @@ class ParserUZ(base.Parser):
                         rates['usd_buy'] = rate.from_string(tag.findChildren('td')[5].getText())
                         rates['usd_sale'] = rate.from_string(tag.findChildren('td')[4].getText())
                         continue
-
                     if re.search(r'EUR', tag.text):
                         rates['eur_buy'] = rate.from_string(tag.findChildren('td')[5].getText())
                         rates['eur_sale'] = rate.from_string(tag.findChildren('td')[4].getText())
                         continue
-
                     if re.search(r'RUB', tag.text):
                         rates['rub_buy'] = rate.from_string(tag.findChildren('td')[5].getText())
                         rates['rub_sale'] = rate.from_string(tag.findChildren('td')[4].getText())
                         continue
-
                 return rates
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -310,10 +254,7 @@ class ParserUZ(base.Parser):
 
     # 8. Parse Aab (web page)
     def parse_aab(self):
-        result = self.fetcher.fetch(
-            "http://www.aab.uz/ru/"
-        )
-
+        result = self.fetcher.fetch("http://www.aab.uz/ru/")
         try:
             context = BeautifulSoup(
                 result,
@@ -324,7 +265,6 @@ class ParserUZ(base.Parser):
             if tags:
                 buy = tags[0].find_all('div', class_='item')
                 sale = tags[1].find_all('div', class_='item')
-
                 return {
                     'usd_buy': rate.from_string(buy[1].text),
                     'usd_sale': rate.from_string(sale[1].text),
@@ -333,7 +273,6 @@ class ParserUZ(base.Parser):
                     'rub_buy': rate.from_string(buy[3].text),
                     'rub_sale': rate.from_string(sale[3].text),
                 }
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -341,17 +280,13 @@ class ParserUZ(base.Parser):
 
     # 9. Parse Trustbank (web page)
     def parse_trustbank(self):
-        result = self.fetcher.fetch(
-            "http://trustbank.uz/ru/services/exchange-rates/"
-        )
-
+        result = self.fetcher.fetch("http://trustbank.uz/ru/services/exchange-rates/")
         try:
             context = BeautifulSoup(result, 'html.parser', parse_only=SoupStrainer(id='currency-archive'))
             tags = context.find_all('div', class_='col-xs-1')
             if tags:
                 buy = tags[2].find_all('div', class_='item')
                 sale = tags[3].find_all('div', class_='item')
-
                 return {
                     'usd_buy': rate.from_string(buy[1].text),
                     'usd_sale': rate.from_string(sale[1].text),
@@ -360,7 +295,6 @@ class ParserUZ(base.Parser):
                     'rub_buy': rate.from_string(buy[4].text),
                     'rub_sale': rate.from_string(sale[4].text),
                 }
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -368,10 +302,7 @@ class ParserUZ(base.Parser):
 
     # 10. Parse Ipakyulibank (web page)
     def parse_ipakyulibank(self):
-        result = self.fetcher.fetch(
-            "https://wi.ipakyulibank.uz/kurs/kurs4.php"
-        )
-
+        result = self.fetcher.fetch("https://wi.ipakyulibank.uz/kurs/kurs4.php")
         try:
             context = BeautifulSoup(result, 'html.parser')
             tags = context.find_all('tr')
@@ -382,19 +313,15 @@ class ParserUZ(base.Parser):
                         rates['usd_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['usd_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                     if re.search(r'EUR', tag.text):
                         rates['eur_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['eur_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                     if re.search(r'RUB', tag.text):
                         rates['rub_buy'] = rate.from_string(tag.findChildren('td')[1].getText())
                         rates['rub_sale'] = rate.from_string(tag.findChildren('td')[2].getText())
                         continue
-
                 return rates
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
@@ -402,28 +329,22 @@ class ParserUZ(base.Parser):
 
     # 11. Parse Savdogarbank (web page)
     def parse_savdogarbank(self):
-        result = self.fetcher.fetch(
-            "http://www.savdogarbank.uz/ru/"
-        )
-
+        result = self.fetcher.fetch("http://www.savdogarbank.uz/ru/")
         try:
             context = BeautifulSoup(result, 'html.parser', parse_only=SoupStrainer('div', {'class': 'b-rates'}))
             tags = context.find_all('tr')
             if tags:
                 buy = tags[2].find_all('td')
                 sale = tags[3].find_all('td')
-
                 return {
                     'usd_buy': rate.from_string(buy[1].text),
                     'usd_sale': rate.from_string(sale[1].text),
                     'eur_buy': rate.from_string(buy[2].text),
                     'eur_sale': rate.from_string(sale[2].text),
-
                     # absent
                     # rates['rub_buy'] = string_to_int(buy[4].text)
                     # rates['rub_sale'] = string_to_int(sale[4].text)
                 }
-
             else:
                 raise base.ParseError("rates not found")
         except Exception as e:
